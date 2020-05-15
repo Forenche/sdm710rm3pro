4c4cd2f6d050d03cf7723a2e444ea193a7eb3c37
Blaster4385 <venkateshchaturvedi12@gmail.com>
2020-05-15 10:14:23 +0530

    Fix for compiling with clang

--- techpack/audio/asoc/codecs/tfa98xx/tfa_dsp.c
+++ techpack/audio/asoc/codecs/tfa98xx/tfa_dsp.c
@@ -2697,7 +2697,7 @@ enum Tfa98xx_Error tfaRunSpeakerCalibration(Tfa98xx_handle_t handle, int profile
     enum Tfa98xx_Error err = Tfa98xx_Error_Ok;
     int calibrateDone, spkr_count = 0;
     /* Avoid warning in user-space */
-    profile=profile;
+    //profile=profile;
 
 #ifdef __KERNEL__ /* Necessary otherwise we are thrown out of operating mode in kernel (because of internal clock) */
     if((strstr(tfaContProfileName(handle, profile), ".cal") == NULL) && (tfa98xx_dev_family(handle) == 2))
@@ -3103,7 +3103,7 @@ enum Tfa98xx_Error tfaRunWaitCalibration(Tfa98xx_handle_t handle, int *calibrate
     return err;
 }
 
-enum tfa_error tfa_start(int next_profile, int *vstep)
+enum Tfa98xx_Error  tfa_start(int next_profile, int *vstep)
 {
     enum Tfa98xx_Error err = Tfa98xx_Error_Ok;
     int dev, devcount = tfa98xx_cnt_max_device();
@@ -3117,8 +3117,6 @@ enum tfa_error tfa_start(int next_profile, int *vstep)
         pr_err("No or wrong container file loaded\n");
         #ifndef VENDOR_EDIT
         /*xiang.fei@PSW.MM.AudioDriver.SmartPA, 2017/09/08,Modify for code error*/
-        return tfa_error_bad_param;
-        #else /* VENDOR_EDIT */
         return Tfa98xx_Error_Bad_Parameter;
         #endif /* VENDOR_EDIT */
     }
@@ -3263,7 +3261,7 @@ error_exit:
     return err;
 }
 
-enum tfa_error tfa_stop(void)
+enum Tfa98xx_Error  tfa_stop(void)
 {
     enum Tfa98xx_Error err = Tfa98xx_Error_Ok;
     int dev, devcount = tfa98xx_cnt_max_device();
@@ -3272,8 +3270,6 @@ enum tfa_error tfa_stop(void)
         pr_err("No or wrong container file loaded\n");
         #ifndef VENDOR_EDIT
         /*xiang.fei@PSW.MM.AudioDriver.SmartPA, 2017/09/08,Modify for code error*/
-        return tfa_error_bad_param;
-        #else /* VENDOR_EDIT */
         return Tfa98xx_Error_Bad_Parameter;
         #endif /* VENDOR_EDIT */
     }
@@ -3332,7 +3328,7 @@ int tfa98xx_reset(Tfa98xx_handle_t handle)
     return err;
 }
 
-enum tfa_error tfa_reset(void)
+enum Tfa98xx_Error  tfa_reset(void)
 {
     enum Tfa98xx_Error err = Tfa98xx_Error_Ok;
     int dev, devcount = tfa98xx_cnt_max_device();
