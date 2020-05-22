b322d09d05e06960b0d6d47b6ad8b4dd30762f4e
Forenche <prahul2003@gmail.com>
2020-05-22 13:29:16 +0000

    add  .travis.yml and build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
-git clone https://github.com/kdrag0n/proton-clang toolchain
+git clone --depth=1 https://github.com/kdrag0n/proton-clang toolchain
 
 export PATH="/home/travis/build/Blaster4385/sdm710rm3pro/toolchain/bin:${PATH}"
 
 export ARCH=arm64
-
-git remote add test https://github.com/AOSiP-Devices/kernel_xiaomi_sdm660.git
-
-git fetch test
-
-git cherry-pick 3b0f5f3c1388d885331726ea0754ec913dc08295
 
 make O=out RMX1851_defconfig
 
